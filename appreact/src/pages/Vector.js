3087d148da5fd6e7a841242b391a8b7ed47f9a3a
administrador <ivanpala21@gmail.com>
2022-02-28 20:50:44 +0100

    actualitza dilluns

--- appreact/src/pages/Vector.js
+++ appreact/src/pages/Vector.js
@@ -1,15 +1,33 @@
 export default function Vector() {
-  let llista = [22, 4, 55, 6, 34, 2, 3, 3];
+  let llista = [22, 4];
+
+  let llista2 = ["Pablo", "Pedro", "Ivan", "Oli"];
 
   let numMayor = Math.max(...llista);
   let numMenor = Math.min(...llista);
+  /*array llista*/
+
+  const llistaprincipal = llista.map((num) => <li>{num}</li>);
+  /*array llista2*/
+
+  const llistaprincipal2 = llista2.map((num) => <li>{num}</li>);
+
   /*afegeix un element nou a un array*/
   llista.splice(2, 0, 2);
   /* treu els elements repetits d'un array */
   const llistaArr = [...new Set(llista)];
+  const eliminarepes = llistaArr.map((num) => <li>{num}</li>);
 
-  const llistarepe = llistaArr.filter((item) => !llista.includes(item));
-  console.log(llistarepe);
+  /* treu els elements NO repetits d'un array */
+
+  const imprimerepes = eliminarepes.filter((value, index) => value !== index);
+
+  /* Concatenar dos arrays */
+
+  let exercici5 = [];
+  for (let i = 0; i < llista.length; i++) {
+    exercici5.push(<li>{llista[i] + llista2[i]} </li>);
+  }
 
   let suma = 0;
   let Mitjana = 0;
@@ -21,6 +39,12 @@ export default function Vector() {
 
   return (
     <>
+      <label>Array</label>
+      <br />
+      <ul>{llistaprincipal}</ul>
+      <label>Array</label>
+      <br />
+      <ul>{llistaprincipal2}</ul>
       <label>comprovar el num major d'un array</label>
       <br />
       {numMayor}
@@ -43,11 +67,14 @@ export default function Vector() {
       <br />
       <label>Treure els repetits en un array</label>
       <br />
-      {llistaArr}
+      <ul>{eliminarepes}</ul>
       <br />
       <label>Treure els repetits en un array</label>
       <br />
-      {llistarepe}
+      {imprimerepes}
+      <label>Concatenar un array</label>
+      <br />
+      <ul>{exercici5}</ul>
     </>
   );
 }
